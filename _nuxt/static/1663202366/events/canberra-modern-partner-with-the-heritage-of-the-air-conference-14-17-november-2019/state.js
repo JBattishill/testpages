4b733b5e3d0c9e61efa4d29681332895cb3822e9
JBattishill <jbattishill91@gmail.com>
2022-09-15 11:21:29 +1000

    test upload nuxt dist folder

---
+++ _nuxt/static/1663202366/events/canberra-modern-partner-with-the-heritage-of-the-air-conference-14-17-november-2019/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1663202366",layout:"default",error:a,serverRendered:true,routePath:"\u002Fevents\u002Fcanberra-modern-partner-with-the-heritage-of-the-air-conference-14-17-november-2019",config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},http:{browserBaseURL:b}}}}(null,"\u002F"));
